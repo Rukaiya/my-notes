6bf433e36c6b298f98d5d71dbcee8d96761707a3
Rukaiya Amin <rukaiyaamin36@gmail.com>
2023-08-10 06:23:49 +0600

    added react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './App.css'
 
 import Header from './components/Header'
 import NotesListPage from './pages/NotesListPage'
+import NotePage from './pages/NotePage'
 
 function App() {
 
   return (
-    <>
-      <Header />
-      <NotesListPage />
-    </>
+    <div>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path='/' exact element={<NotesListPage/>} />
+          <Route path='/notes' component={<NotePage/>} />
+        </Routes>
+      </Router>
+    </div>
   )
 }
 
