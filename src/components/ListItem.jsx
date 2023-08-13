d88c93852586f90a896c81a28a3948a7c8ae02e4
Rukaiya <rukaiyaamin36@gmail.com>
2023-08-13 14:25:42 +0600

    fixed id issue

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const ListItem = ({ note }) => {
   return (
     <div>
-      {note.body}
+      <Link to={`/note/${note.id}`}>{note.body} </Link>
     </div>
   )
 }
--- src/pages/NotePage.jsx
+++ src/pages/NotePage.jsx
@@ -2,13 +2,11 @@ import { useParams } from 'react-router-dom'
 import notes from '../assets/data'
 
 const NotePage = () => {
-  let noteId = useParams();
-  // let note = notes.find(note => note.id === Number(noteId))
-  console.log(noteId);
-  console.log(Number(noteId));
+  let { id: noteId } = useParams();
+  let note = notes.find(note => note.id === Number(noteId))
   return (
     <div>
-      {/* <p>{note.body}</p> */}
+      <p>{note.body}</p>
     </div>
   )
 }
